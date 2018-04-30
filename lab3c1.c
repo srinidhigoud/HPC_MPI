16fca93e1edc4aad184c3ea7dce79244f39f353b
srinidhigoud <srinidhi.goud29@gmail.com>
2018-04-30 04:09:08 -0400

    run

--- lab3c1.c
+++ lab3c1.c
@@ -13,10 +13,10 @@ int main(int argc, char *argv[]){
     MPI_Init(&argc,&argv); 
     MPI_Comm_size(MPI_COMM_WORLD, &world_size); 
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    MPI_Request request[(world_size-1)];
-    MPI_Status status[(world_size-1)];
-     MPI_Request request2[(world_size-1)];
-    MPI_Status status2[(world_size-1)];
+    MPI_Request request[2*(world_size-1)];
+    MPI_Status status[2*(world_size-1)];
+    //  MPI_Request request2[(world_size-1)];
+    // MPI_Status status2[(world_size-1)];
     double *buff;
     // printf("here\n");
     printf("%d %d\n", world_size, world_rank);
@@ -46,9 +46,8 @@ int main(int argc, char *argv[]){
     // printf("here3\n");
     if(world_rank>0) MPI_Irecv(buff+(world_rank-1)*C*H*W, C*H*W, MPI_DOUBLE, world_rank, 123+world_rank, MPI_COMM_WORLD, &request[world_rank-1]);
     // printf("here4q\n");
-    MPI_Isend(I_sub, C*H*W, MPI_DOUBLE, 0, 123+world_rank, MPI_COMM_WORLD, &request2[world_rank-1]);
-    MPI_Waitall(world_size-1, request, status); 
-    MPI_Waitall(world_size-1, request2, status2); 
+    MPI_Isend(I_sub, C*H*W, MPI_DOUBLE, 0, 123+world_rank, MPI_COMM_WORLD, &request[world_rank-1 + world_size-1]);
+    MPI_Waitall(2*(world_size-1), request, status); 
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
     for(int idx=0;idx<world_size-1;idx++){
