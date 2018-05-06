db6a473bdba0a56da36cf944091436f007694a6c
srinidhigoud <srinidhi.goud29@gmail.com>
2018-05-05 22:45:10 -0400

    run

--- lab3c1.c
+++ lab3c1.c
@@ -4,7 +4,7 @@
 #include <assert.h>
 #define H 2 
 #define W 2
-#define C 1
+#define C 3
 
 int main(int argc, char *argv[]){
     int world_rank, world_size; 
@@ -64,7 +64,7 @@ int main(int argc, char *argv[]){
         for(int i=0;i<C;i++){
             for(int j=0;j<H;j++) {
                 for(int k=0;k<W;k++) {
-                    I_sub[i*H*W+j*W+k] = i*(j+k);
+                    I_sub[i*H*W+j*W+k] = world_rank + i*(j+k);
                     // printf("%lf ",I_sub[i*H*W+j*W+k]);
                 }
                 // printf("\n");
