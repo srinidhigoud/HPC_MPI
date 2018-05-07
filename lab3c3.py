8edc37a128b8169a9ac78eb771e80329949dc88d
srinidhigoud <srinidhi.goud29@gmail.com>
2018-05-06 23:18:42 -0400

    run

--- lab3c3.py
+++ lab3c3.py
@@ -168,7 +168,7 @@ def run(rank, size, dataset_loader, batchSize, model, optimizer, criterion):
     dist.all_reduce(loss_w, op=dist.reduce_op.SUM, group=0)
     dist.all_reduce(numberOfSamples, op=dist.reduce_op.SUM, group=0)
 
-    return loss_w, numberOfSamples
+    return loss_w[0], numberOfSamples[0]
 
 
 
@@ -192,7 +192,7 @@ def main(rank, size):
     if rank == 0:
         print("Final Weighted Loss - ",(weighted_loss/numberOfSamples))
         print("The time is - ",t0)
-    test_dataset = data(csv_file = '/scratch/am9031/CSCI-GA.3033-023/lab3/kaggleamazon/test.csv', root_dir = '/scratch/am9031/CSCI-GA.3033-023/lab3/kaggleamazon/train-jpg/',transform = data_transform)
+    # test_dataset = data(csv_file = '/scratch/am9031/CSCI-GA.3033-023/lab3/kaggleamazon/test.csv', root_dir = '/scratch/am9031/CSCI-GA.3033-023/lab3/kaggleamazon/train-jpg/',transform = data_transform)
 
 
 if __name__ == "__main__":
