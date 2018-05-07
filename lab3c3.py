ad22f2ba1d9462d0af165048bf646a822d0ad919
srinidhigoud <srinidhi.goud29@gmail.com>
2018-05-06 22:22:00 -0400

    run

--- lab3c3.py
+++ lab3c3.py
@@ -163,12 +163,12 @@ def run(rank, size, dataset_loader, batchSize, model, optimizer, criterion):
 
     print('Rank ', dist.get_rank(), ', epoch_loss ', epoch_loss, ', number of samples ', numberOfSamples)
 
-    loss_w = torch.Tensor(epoch_loss * numberOfSamples)
-    numberOfSamples = torch.Tensor(numberOfSamples)
+    # loss_w = torch.Tensor(epoch_loss * numberOfSamples)
+    # numberOfSamples = torch.Tensor(numberOfSamples)
     # dist.all_reduce(loss_w, op=dist.reduce_op.SUM, group=0)
     # dist.all_reduce(numberOfSamples, op=dist.reduce_op.SUM, group=0)
 
-    return loss_w, numberOfSamples
+    # return loss_w, numberOfSamples
 
 # def run(rank, size, dataset, model, optimizer, criterion):
     
@@ -215,19 +215,19 @@ def main(rank, size):
     train_dataset = data(csv_file = '/scratch/am9031/CSCI-GA.3033-023/lab3/kaggleamazon/train.csv', root_dir = '/scratch/am9031/CSCI-GA.3033-023/lab3/kaggleamazon/train-jpg/',transform = data_transform)
     train_set, bsz = partition_dataset(train_dataset)
     t0 = time.monotonic()
-    weighted_loss, numberOfSamples = run(rank, size, train_set,bsz, net, optimizer, criterion)
-    t0 = time.monotonic()-t0
-    if rank == 0:
-        print("Final Weighted Loss - ",(weighted_loss/numberOfSamples))
-        print("The time is - ",t0)
-    test_dataset = data(csv_file = '/scratch/am9031/CSCI-GA.3033-023/lab3/kaggleamazon/test.csv', root_dir = '/scratch/am9031/CSCI-GA.3033-023/lab3/kaggleamazon/train-jpg/',transform = data_transform)
+    run(rank, size, train_set,bsz, net, optimizer, criterion)
+    # t0 = time.monotonic()-t0
+    # if rank == 0:
+    #     print("Final Weighted Loss - ",(weighted_loss/numberOfSamples))
+    #     print("The time is - ",t0)
+    # test_dataset = data(csv_file = '/scratch/am9031/CSCI-GA.3033-023/lab3/kaggleamazon/test.csv', root_dir = '/scratch/am9031/CSCI-GA.3033-023/lab3/kaggleamazon/train-jpg/',transform = data_transform)
 
 
 if __name__ == "__main__":
     
     # dist.init_process_group(backend="mpi", world_size=int(sys.argv[1]))
 
-    dist.init_process_group(backend="mpi")
+    dist.init_process_group(backend="mpi", world_size=4)
     size = dist.get_world_size()
     rank = dist.get_rank() 
 
