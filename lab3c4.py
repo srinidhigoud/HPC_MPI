e4f0a3942cded931ecbff8e90de6d935f73148e6
srinidhigoud <srinidhi.goud29@gmail.com>
2018-05-08 01:59:32 -0400

    run

--- lab3c4.py
+++ lab3c4.py
@@ -184,7 +184,7 @@ def runServer(model, optimizer):
     torch.manual_seed(1234)
     # for param in model.parameters():
     #     param.grad.data = 0
-    model.grad.data.zero_()
+    # model.grad.data.zero_()
 
     dist.recv(tag)
     src = tag[0]
