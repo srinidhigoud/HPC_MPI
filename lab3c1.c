8a9cabc0ad42d60f93b37ddcd9b22562ee65408e
srinidhigoud <srinidhi.goud29@gmail.com>
2018-05-09 02:45:49 -0400

    run

--- lab3c1.c
+++ lab3c1.c
@@ -46,9 +46,9 @@ int main(int argc, char *argv[]){
 
         gettimeofday(&t2, NULL);
         
-        long totalTime = (t2.tv_sec*1e6 + t2.tv_usec) - (t1.tv_sec*1e6 + t1.tv_usec);
+        double totalTime = (t2.tv_sec*1e6 + t2.tv_usec) - (t1.tv_sec*1e6 + t1.tv_usec);
         printf("\n C1 \n");
-        printf("%4.3lf \n",totalTime/1000);
+        printf("%4.3lf \n",totalTime);
         printf("%4.3lf,",checksum/(world_size-1));
         
 
