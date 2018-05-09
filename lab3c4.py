6835ed389edb1a7d70d32e16c6f0d996793834fc
srinidhigoud <srinidhi.goud29@gmail.com>
2018-05-08 22:21:22 -0400

    run

--- lab3c4.py
+++ lab3c4.py
@@ -159,8 +159,8 @@ def runWorker(dataset, criterion, group):
             for param in model.parameters():
                 dist.recv(tensor = param.data, src = 0)
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch, batch_idx * len(data), len(train_set.dataset), 100. * batch_idx / len(train_set), loss.item()))
-        dist.send(tensor = torch.Tensor([0]),dst = 0)
         dist.barrier(group)
+        dist.send(tensor = torch.Tensor([0]),dst = 0)
         for param in model.parameters():
             dist.recv(tensor = param.data, src = 0)
         
